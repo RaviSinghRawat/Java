62889d6299697e6fa8a853adb2716d3af68e4acc
RaviSinghRawat <ravirawat68304@gmail.com>
2023-05-03 09:05:35 +0530

    Inoutnumber

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Demo 
+{
+public static void main ( String arr[])
+{
+int a;
+System.out.println("enter the number");
+Scanner ob = new Scanner(System.in);
+a = ob.nextInt();
+System.out.println("you entered" + a);
+}
+}
